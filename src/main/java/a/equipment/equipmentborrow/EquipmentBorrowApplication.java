fdbacbc5a1d58351efde6752479791a35619d9c2
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-08-23 20:52:08 +0900

    Initial commit

---
+++ src/main/java/a/equipment/equipmentborrow/EquipmentBorrowApplication.java
@@ -0,0 +1,13 @@
+package a.equipment.equipmentborrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipmentBorrowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EquipmentBorrowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/a/equipment/equipmentborrow/EquipmentBorrowApplicationTests.java
@@ -0,0 +1,13 @@
+package a.equipment.equipmentborrow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentBorrowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
